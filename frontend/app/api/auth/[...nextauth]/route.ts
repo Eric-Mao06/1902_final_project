55cd9739dc6e5fb272341be0562f140a46c04759
DavidBShan <davidbshan@gmail.com>
2024-12-31 14:31:33 -0500

    deleted typscript next.config.ts

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -13,7 +13,6 @@ const handler = NextAuth({
     async signIn({ user }) {
       // Only allow @upenn.edu email addresses
       const emailDomain = user.email?.split('@')[1];
-      console.log("WHY",user.email);
       if (user.email === "davidbshan@gmail.com") {
         return true;
       }
